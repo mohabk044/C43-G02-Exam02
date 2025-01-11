c7f07cc6429363672edcdb9bf1dd3132f830e45c
Mohab Khaled <mohabk044@gmail.com>
2025-01-11 15:40:48 +0200

    ExamSystem

---
+++ ExamSystem/MCQ-Question.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExaminationSystem
+{
+    public class MCQ_Question : Question
+    {
+        public override string Header { get; set; }
+        public override string Body { get; set; }
+        public override int Mark { get; set; }
+        public override string ToString()
+        => base.ToString() + "  MCQ Question";
+        public override void ShowQuestion()
+        {
+            Console.WriteLine(this.ToString());
+            for (int i = 0; i < Answers.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}. {Answers[i].answer}");
+            }
+        }
+    }
+}
---
+++ ExamSystem/PracticalExam.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExaminationSystem
+{
+    public class PracticalExam : Exam
+    {
+        public override DateTime ExamTime { get; set; }
+        public override int QusetinNumber { get; set; }
+
+
+
+        public override void ShowExam()
+        {
+            Console.WriteLine("--------------- Practical Exam ---------------");
+            Console.WriteLine();
+            foreach (var question in Questions)
+            {
+                question.ShowQuestion();
+
+                Console.WriteLine();
+
+                Console.WriteLine("----------------------------------");
+
+                Console.WriteLine();
+            }
+        }
+        public void CorrectAnswers()
+        {
+            foreach (var item in Questions)
+            {
+
+                Console.WriteLine($"the Question : {item.Body}");
+                if (item.CorrectAnswers != null)
+                {
+                    Console.WriteLine($"the correct Answer is {item.CorrectAnswers.answer}");
+                }
+                else
+                {
+                    Console.WriteLine("no answers available ");
+                }
+                Console.WriteLine();
+                Console.WriteLine("---------");
+                Console.WriteLine();
+            }
+        }
+
+
+
+    }
+}
